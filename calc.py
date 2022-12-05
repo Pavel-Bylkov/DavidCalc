636932cd803d22d13de469f1c7efa9183af33bf9
unknown <nsk-adm@nsk1-rez-01.licey22.local>
2022-12-05 08:23:32 +0700

    add buttons

--- calc.py
+++ calc.py
@@ -13,14 +13,38 @@ display = QLabel("0123456789")
 box.addWidget(display, alignment=Qt.AlignRight)
 
 line = QHBoxLayout()  # для горизонтального размещения виджетов
-btn1 = QPushButton("1")
-line.addWidget(btn1)
-btn2 = QPushButton("2")
-line.addWidget(btn2)
-btn3 = QPushButton("3")
-line.addWidget(btn3)
+btnPerc = QPushButton("%")
+line.addWidget(btnPerc)
+btnCE = QPushButton("CE")
+line.addWidget(btnCE)
+btnC = QPushButton("C")
+line.addWidget(btnC)
+btnDel = QPushButton("<")
+line.addWidget(btnDel)
 
 box.addLayout(line)
+
+line2 = QHBoxLayout()  # для горизонтального размещения виджетов
+btn1x = QPushButton("1/x")
+line2.addWidget(btn1x)
+btnx2 = QPushButton("x2")
+line2.addWidget(btnx2)
+btnSQRT = QPushButton("xv2")
+line2.addWidget(btnSQRT)
+btnDiv = QPushButton("/")
+line2.addWidget(btnDiv)
+
+box.addLayout(line2)
+
+line3 = QHBoxLayout()  # для горизонтального размещения виджетов
+btn1 = QPushButton("1")
+line3.addWidget(btn1)
+btn2 = QPushButton("2")
+line3.addWidget(btn2)
+btn3 = QPushButton("3")
+line3.addWidget(btn3)
+
+box.addLayout(line3)
 
 win.setLayout(box)
 
