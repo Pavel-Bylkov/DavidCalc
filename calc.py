475f5c8d508ff02eacc4d68e01a833284554dd47
dlubomirsky <david@lubomirsky.com>
2022-12-04 20:36:25 -0500

    Add a dew buttons

--- calc.py
+++ calc.py
@@ -37,14 +37,40 @@ line2.addWidget(btnDiv)
 box.addLayout(line2)
 
 line3 = QHBoxLayout()  # для горизонтального размещения виджетов
-btn1 = QPushButton("1")
-line3.addWidget(btn1)
-btn2 = QPushButton("2")
-line3.addWidget(btn2)
-btn3 = QPushButton("3")
-line3.addWidget(btn3)
+btn7 = QPushButton("7")
+line3.addWidget(btn7)
+btn8 = QPushButton("8")
+line3.addWidget(btn8)
+btn9 = QPushButton("9")
+line3.addWidget(btn9)
+btnX = QPushButton("X")
+line3.addWidget(btnX)
 
 box.addLayout(line3)
+
+line4 = QHBoxLayout()
+btn4 = QPushButton("4")
+line4.addWidget(btn4)
+btn5 = QPushButton("5")
+line4.addWidget(btn5)
+btn6 = QPushButton("6")
+line4.addWidget(btn6)
+btnmin = QPushButton("-")
+line4.addWidget(btnmin)
+
+box.addLayout(line4)
+
+line5 = QHBoxLayout()
+btn1 = QPushButton("1")
+line5.addWidget(btn1)
+btn2 = QPushButton("2")
+line5.addWidget(btn2)
+btn3 = QPushButton("3")
+line5.addWidget(btn3)
+btnplus = QPushButton("+")
+line5.addWidget(btnplus)
+
+box.addLayout(line5)
 
 win.setLayout(box)
 
