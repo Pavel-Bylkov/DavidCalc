3260a5ef314c56bc7b694842952fd1735d1f6ac2
unknown <nsk-adm@nsk1-rez-01.licey22.local>
2022-12-12 08:26:32 +0700

    add scroll

--- calc.py
+++ calc.py
@@ -1,5 +1,12 @@
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import Qt
+from display import Display
+
+class MyButton(QPushButton):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.setFixedHeight(45)
+
 
 app = QApplication([])
 win = QWidget()
@@ -9,77 +16,77 @@ win.resize(300, 400)
 
 box = QVBoxLayout()  # для вертикального размещения виджетов
 
-display = QLabel("0123456789")
+display = Display()  # подключаем свой
 box.addWidget(display, alignment=Qt.AlignRight)
 
 line = QHBoxLayout()  # для горизонтального размещения виджетов
-btnPerc = QPushButton("%")
+btnPerc = MyButton("%")
 line.addWidget(btnPerc)
-btnCE = QPushButton("CE")
+btnCE = MyButton("CE")
 line.addWidget(btnCE)
-btnC = QPushButton("C")
+btnC = MyButton("C")
 line.addWidget(btnC)
-btnDel = QPushButton("«")
+btnDel = MyButton("«")
 line.addWidget(btnDel)
 
 box.addLayout(line)
 
 line2 = QHBoxLayout()  # для горизонтального размещения виджетов
-btnreciprocal = QPushButton("1÷x")
+btnreciprocal = MyButton("1÷x")
 line2.addWidget(btnreciprocal)
-btnx2 = QPushButton("xª")
+btnx2 = MyButton("xª")
 line2.addWidget(btnx2)
-btnSQRT = QPushButton("√")
+btnSQRT = MyButton("√")
 line2.addWidget(btnSQRT)
-btnDiv = QPushButton("÷")
+btnDiv = MyButton("÷")
 line2.addWidget(btnDiv)
 
 box.addLayout(line2)
 
 line3 = QHBoxLayout()  # для горизонтального размещения виджетов
-btn7 = QPushButton("7")
+btn7 = MyButton("7")
 line3.addWidget(btn7)
-btn8 = QPushButton("8")
+btn8 = MyButton("8")
 line3.addWidget(btn8)
-btn9 = QPushButton("9")
+btn9 = MyButton("9")
 line3.addWidget(btn9)
-btnX = QPushButton("×")
+btnX = MyButton("×")
 line3.addWidget(btnX)
 
 box.addLayout(line3)
 
 line4 = QHBoxLayout()
-btn4 = QPushButton("4")
+btn4 = MyButton("4")
 line4.addWidget(btn4)
-btn5 = QPushButton("5")
+btn5 = MyButton("5")
 line4.addWidget(btn5)
-btn6 = QPushButton("6")
+btn6 = MyButton("6")
 line4.addWidget(btn6)
-btnmin = QPushButton("-")
+btnmin = MyButton("-")
 line4.addWidget(btnmin)
 
 box.addLayout(line4)
 
 line5 = QHBoxLayout()
-btn1 = QPushButton("1")
+btn1 = MyButton("1")
 line5.addWidget(btn1)
-btn2 = QPushButton("2")
+btn2 = MyButton("2")
 line5.addWidget(btn2)
-btn3 = QPushButton("3")
+btn3 = MyButton("3")
 line5.addWidget(btn3)
-btnplus = QPushButton("+")
+btnplus = MyButton("+")
 line5.addWidget(btnplus)
 
 box.addLayout(line5)
 
 line6 = QHBoxLayout()
-btnposneg = QPushButton("+/-")
+btnposneg = MyButton("+/-")
 line6.addWidget(btnposneg)
-btn0 = QPushButton("0")
+btn0 = MyButton("0")
 line6.addWidget(btn0)
-btndot= QPushButton(".")
+btndot= MyButton(".")
 line6.addWidget(btndot)
-btnequal = QPushButton("=")
+btnequal = MyButton("=")
 line6.addWidget(btnequal)
 
 
---
+++ display.py
@@ -0,0 +1,47 @@
+#подключаем модуль с направляющими линиями
+from PyQt5.QtCore import Qt
+#подключаем необходимые виджеты
+from PyQt5.QtWidgets import (QWidget, QLabel, QVBoxLayout,
+                               QScrollArea)
+from PyQt5.QtGui import QFont
+
+# class for scrollable label
+class Display(QScrollArea):
+
+    # constructor
+    def __init__(self, *args, **kwargs):
+        QScrollArea.__init__(self, *args, **kwargs)
+
+        # making widget resizable
+        self.setWidgetResizable(True)
+        self.setFixedWidth(390)
+        self.setFixedHeight(90)
+
+        # making qwidget object
+        content = QWidget(self)
+        self.setWidget(content)
+
+        # vertical box layout
+        lay = QVBoxLayout(content)
+
+        # creating label
+        self.label = QLabel(content)
+        self.label.setFont(QFont('Arial', 24))
+        self.label.setText("0")
+
+        # setting alignment to the text
+        self.label.setAlignment(Qt.AlignRight | Qt.AlignTop)
+
+        # making label multi-line
+        # self.label.setWordWrap(True)
+
+        # adding label to the layout
+        lay.addWidget(self.label)
+
+    # the setText method
+    def setText(self, text):
+        # setting text to the label
+        self.label.setText(text)
+
+    def text(self):
+        return self.label.text()
